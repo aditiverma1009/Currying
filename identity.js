1be25e114d48738c1cfd683c29c7586ad802130f
aditiverma1009 <aditiverma1009@gmail.com>
2018-01-23 15:10:30 +0530

    identity question

--- identity.js
+++ identity.js
@@ -1,6 +1,6 @@
-const identity = arg => arg;
-module.exports = identity;
+const identityfunc = arg => arg;
+module.exports = identityfunc;
 
 const arg = 1;
-const result = identity(arg);
+const result = identityfunc(arg);
 console.log(result);
--- identity.test.js
+++ identity.test.js
@@ -1,10 +1,6 @@
-const identityTest = require('/identity');
+const identityTest = require('./identity');
 
 const value = 0;
-test('This is a test case', () => {
-  expect(identity(value).toBe(value));
-});
-
-test('Testing for all 0 strikes and hence the result must be 0', () => {
-  expect(calculatedScore(inputAllZero).toBe(0));
+test('This is a test case to check if sent argument and recieved arguments are the same', () => {
+  expect(identityTest(value).toBe(value));
 });
